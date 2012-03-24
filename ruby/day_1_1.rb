d83561a2e733abc8b1009f6ec4c3255cc5e55224
Matt Vanderpol <matt@vanderpol.net>
2012-03-23 20:19:46 -0700

    Ruby work

--- ruby/day_1_1.rb
+++ ruby/day_1_1.rb
@@ -4,4 +4,4 @@ puts "Hello, Ruby".index("Ruby")
 
 10.times { puts "Matt Vanderpol" }
 
-(1..10).to_a.each { |n| puts "This is sentence number #{n}" }
+(1..10).each { |n| puts "This is sentence number #{n}" }
