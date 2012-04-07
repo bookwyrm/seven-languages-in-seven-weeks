4b430d5caa98f48437de38c0bc9370fa22428959
Matt Vanderpol <matt@vanderpol.net>
2012-04-07 10:58:15 -0700

    More idiomatic ruby

--- ruby/day_2_1.rb
+++ ruby/day_2_1.rb
@@ -1,6 +1,6 @@
 a = []
 i = -1
-(1..16).each { |n| n % 4 == 1 ? a[i=i+1] = [n] : a[i].push(n) }
+(1..16).each { |n| n % 4 == 1 ? a[i=i+1] = [n] : a[i] << (n) }
 p a
 
 (1..16).each_slice(4) { |a| p a }
